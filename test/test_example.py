aebb06310d4e4ed5a50d80b1c9538b81fe6f9fd7
Alena Yanushevskaya <qahelping@gmail.vom>
2025-10-22 21:58:00 +0300

    add new test

--- test/test_example.py
+++ test/test_example.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     opts = Options()
-    opts.add_argument("--headless=new")
-    opts.add_argument("--window-size=1280,900")
+    # opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=640,900")
     driver = webdriver.Chrome(options=opts)
     yield driver
 
@@ -15,7 +15,14 @@ def driver():
 
 
 def test_selenium_web(driver):
-    url = "https://www.selenium.dev"
+    url = "https://www.selenium.dev/"
     driver.get(url)
     assert driver.title == "Selenium"
+    assert driver.current_url == url
+
+
+def test_pytest(driver):
+    url = "https://docs.pytest.org/en/stable/"
+    driver.get(url)
+    assert driver.title == "pytest documentation"
     assert driver.current_url == url

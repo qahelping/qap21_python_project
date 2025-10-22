93c83ae3b40e7b4cda3dd64efd1aa4ab9c19c6fc
Alena Yanushevskaya <qahelping@gmail.vom>
2025-10-22 20:18:05 +0300

    init project

--- test/test_example.py
+++ test/test_example.py
@@ -1,22 +1,21 @@
-from selenium.webdriver.chrome.options import Options
 import pytest
-import time
-
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
 @pytest.fixture
 def driver():
-    opts = Options(); opts.add_argument("--headless=new")
+    opts = Options()
+    opts.add_argument("--headless=new")
     opts.add_argument("--window-size=1280,900")
     driver = webdriver.Chrome(options=opts)
     yield driver
 
     driver.quit()
+
+
 def test_selenium_web(driver):
-  url = "https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/https://www.selenium.dev/"
-  driver.get(url)
-  assert driver.title == "Selenium1"
-  assert driver.current_url == url
-
-
-
-
+    url = "https://www.selenium.dev"
+    driver.get(url)
+    assert driver.title == "Selenium"
+    assert driver.current_url == url
